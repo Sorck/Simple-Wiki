c3588ba06506d376a2406e192b337dcc21196824
James Robson <git@secondsphere.org>
2012-08-21 00:42:05 +0100

    ! playing with the menu Signed-off-by:James Robson <git@secondsphere.org>

--- Controllers/AbstractWikiController.php
+++ Controllers/AbstractWikiController.php
@@ -8,6 +8,7 @@ abstract class AbstractWikiController extends Controller
 {
 	protected $_page_name = null;
 	protected $_page = null;
+	protected $_route = null;
 	
 	/**
 	 * 
@@ -21,9 +22,9 @@ abstract class AbstractWikiController extends Controller
 		}
 		else
 		{
-			$route = 'wiki' . ($route ? '/' . $route : '');
+			$this->_route = 'wiki' . ($route ? '/' . $route : '');
 			// get our page name
-			$this->_page_name = $this->_getPathData($route);
+			$this->_page_name = $this->_getPathData($this->_route);
 			if($load_page)
 			{
 				// load our page storage
@@ -40,6 +41,19 @@ abstract class AbstractWikiController extends Controller
 	 */
 	protected function _render($template = "wiki_page", $context = array())
 	{
+		Storage\Menu::set('urlname', $this->_page_name);
+		$page_menu_data = new Storage\Menu('');
+		$page_menu_data[] = new Storage\Menu('Permalink');
+		$page_menu_data[] = new Storage\Menu('Page Toolbox', 'wrench');
+		$page_menu_data[1][] = new Storage\Menu('Edit', 'pencil', '/wiki/edit/{page_name}');
+		$page_menu_data[1][] = new Storage\Menu('Move', 'share-alt', '/wiki/move/{page_name}');
+		$page_menu_data[1][] = new Storage\Menu('Protect', 'lock', '/wiki/protect/{page_name}');
+		$page_menu_data[1][] = new Storage\Menu('Remove', 'trash', '/wiki/remove/{page_name}');
+		$page_menu_data[1][] = new Storage\Menu('History', 'list', '/wiki/history/{page_name}');
+		$page_menu_data['data_Page Toolbox']['data_Edit']['active'] = true;
+		$page_menu_data[] = new Storage\Menu('Share', 'share');
+		$page_menu_data[2][] = new Storage\Menu('QRCode', 'qrcode', '/wiki/qrcode/{page_name}');
+		$page_menu_data[2][] = new Storage\Menu('E-Mail to friend', 'envelope', 'mailto:?subject=smWiki+-+' . $this->_page_name . '&body=' . urlencode(Settings::URL . '/' . $this->_route . '/' . $this->_page_name));
 		return $this->_getParentModule()->render($template, array_merge(array(
 			'page' => $this->_page,
 			// @todo Get this data from the database...
@@ -47,8 +61,9 @@ abstract class AbstractWikiController extends Controller
 				array(
 					'title' => 'Home',
 					'link' => Settings::URL . '/wiki/Main_Page',
-				)
-			)), $context));
+				),
+			),
+			'page_menu_data' => $page_menu_data), $context));
 	}
 	
 	/**
